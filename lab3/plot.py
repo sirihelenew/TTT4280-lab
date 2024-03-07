b586c719cc6b598897339ff7315ff0a58a98e559
Sunnivar <sunrost@online.no>
2024-03-07 09:23:54 +0100

    plots, snr, std

--- lab3/plot.py
+++ lab3/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-with open('outputny/eggan28.txt', 'r') as f:
+with open('data/eggan1.txt', 'r') as f:
     r = []
     g = []
     b = []
@@ -19,7 +19,7 @@ t = np.arange(0,10, 10/len(g))
 
 g_hann = np.hanning(len(g))*g
 
-n_pad = 2**15
+n_pad = 2**16
 g_pad = np.pad(g_hann, (0, n_pad - len(g)), 'constant')
     
 autocorr = np.correlate(g_pad, g_pad, mode='full')  
@@ -28,11 +28,24 @@ lags = np.arange(-n_pad + 1, n_pad,1)
 fft = np.fft.fft(g_pad, n_pad)
 freq = np.fft.fftfreq(n_pad)
     
-#plt.plot(t, b)
-#plt.plot(freq*30*60, np.abs(fft))
+# plt.plot(t, g, color = "green")
+# plt.plot(t, b, color = "blue")
+# plt.plot(t, r, color = "red")
+plt.plot(freq*30*60, np.abs(fft))
 #plt.plot(lags, autocorr)
-#plt.xlim(-1000,1000)
-#plt.show()
+plt.xlim(0,200)
+plt.show()
+
+
+std_signal = abs(np.std(fft))
+max_signal = abs(np.argmax(fft))
+
+SNR = max_signal/std_signal
+
+
+print("SNR: ", SNR)
+
+
 
 for i in range(-5, 5):
     autocorr[n_pad + i] = 0
@@ -41,5 +54,5 @@ index = np.argmax(autocorr)
 puls = 30*60/lags[index]
 
 puls_fft = 30*60*freq[np.argmax(fft)]
-print("Puls med autokorrelasjon: ", -1*puls)
-print("Puls med FFT: ", -1*puls_fft)
+print("Puls med autokorrelasjon: ", abs(puls))
+print("Puls med FFT: ", abs(puls_fft))
--- lab3/std.py
+++ lab3/std.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-puls = []
-puls_klokke = [] 
+puls = [80.035, 79.959, 83.606, 88.165, 94.373]
+puls_auto = [78.26, 75, 81.818, 81.818, 90]
+puls_klokke = [80, 75, 80, 85, 85] 
 
 puls_klokke_snitt = 0
 
@@ -15,9 +16,9 @@ puls_std1 = np.std(puls)
 puls_var = 0
 
 for i in range(len(puls)):
-    puls_var += (puls[i]-puls_klokke_snitt)^2/len(puls)
+    puls_var += (puls[i]-puls_klokke_snitt)**2/len(puls)
     
-puls_std2 = puls_var/2
+puls_std2 = np.sqrt(puls_var)
 
 print("Standardavvik funnet med np.std(): ", puls_std1)
 print("Gjennomsnittelig puls målt med klokke: ", puls_klokke_snitt)
